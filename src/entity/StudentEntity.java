93327d1a06a5d0b6bdf3a954109ee264be2715e7
kosala509 <sirisenagkg.20@uom.lk>
2023-08-26 19:57:34 +0530

    Student Table dev 2

--- src/entity/StudentEntity.java
+++ src/entity/StudentEntity.java
@@ -1,12 +1,20 @@
 package entity;
 
+import java.util.*;
+
+import javax.persistence.CollectionTable;
 import javax.persistence.Column;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.Table;
 
+import org.hibernate.annotations.CreationTimestamp;
+
+import entity.embeded.StudentName;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -22,12 +30,27 @@ import lombok.NoArgsConstructor;
 @ToString
 
 public class StudentEntity {
+    // ID Attribute
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    // Attribute
     @Column(name = "nic", nullable = false)
     private String nic;
+
+    private StudentName name;
+
+    @ElementCollection
+    @CollectionTable(
+        name = "student_mobile",
+        joinColumns = @JoinColumn(name = "student_id")
+    )
+    private List<String> mobiles;
+
+    @CreationTimestamp
+    @Column(name = "create_date", nullable = false)
+    private Date createDate;
 
 }
